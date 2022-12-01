832f82b02df402013b0d945178dd32b31f201a86
TiagoNora <tiagonora2002@gmail.com>
2022-12-01 18:00:56 +0000

    Update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+
 insert into users (id, username,email, password) values (1,'Álvaro','alvaro@gmail.com', 'gostei123');
 insert into users (id, username,email, password) values (2,'Tiago','tiago@gmail.com', 'gostei123');
 insert into users (id, username,email, password) values (3,'Mariana','mariana@gmail.com', 'gostei123');
@@ -8,4 +9,3 @@ insert into roles(id,authority) values (2,'MODERATOR')
 insert into user_roles(user_id,role_id) values(1,1)
 insert into user_roles(user_id,role_id) values(2,2)
 insert into user_roles(user_id,role_id) values(3,2)
-
